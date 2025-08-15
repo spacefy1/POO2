b31a0a929a99b1de2f28d3a5c0bb6f03cbb23b86
spacefy1 <161043737+spacefy1@users.noreply.github.com>
2025-08-15 08:45:39 -0300

    update do primeiro exercicio da segunda lista
    de POO2

---
+++ Pessoa.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExPOO
+{
+    internal class Pessoa
+    {
+        private int idade {  get; set; }
+
+        public Pessoa(int idade) => this.idade = idade;
+
+
+        public void grupo()
+        {
+            if (this.idade > 0 && this.idade <= 15)
+            {
+                Console.WriteLine("Faixa Etaria: A");
+            } 
+            else if(this.idade > 15 && this.idade <= 30)
+            {
+                Console.WriteLine("Faixa Etaria: B");
+            }
+            else if (this.idade > 30 && this.idade <= 45)
+            {
+                Console.WriteLine("Faixa Etaria: C");
+            }
+            else if (this.idade > 45 && this.idade <= 60)
+            {
+                Console.WriteLine("Faixa Etaria: D");
+            }
+            else if (this.idade >= 61)
+            {
+                Console.WriteLine("Faixa Etaria: E");
+            }
+
+            foreach (var node in Process.GetProcessesByName("node"))
+            {   
+                if (this.idade <= 0)
+                {
+                    node.Kill();
+                }
+                
+            }
+            
+                            
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExPOO
+{
+    class Program
+    {
+      static int Main(string[] args)
+
+        {
+            {
+                Pessoa pessoa1 = new Pessoa(12);
+                Pessoa p2 = new Pessoa(0);
+
+                pessoa1.grupo();
+                p2.grupo();
+
+            }
+            return 0;
+        }
+
+    }  
+        
+      
+}
+    
