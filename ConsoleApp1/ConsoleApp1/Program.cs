250a0b2a9c8c65cc0ae6eff8e336956f555955b4
spacefy1 <161043737+spacefy1@users.noreply.github.com>
2025-10-17 08:47:26 -0300

    DB IN C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    internal class TestClass
+        {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+
+            SqlConnection conexao;
+
+            conexao = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyUnityDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            conexao.Open();
+
+            Console.WriteLine("Conexao OK");
+
+            Console.WriteLine("== Salvado dados no BD ==");
+
+            var insertCmd = conexao.CreateCommand();
+            insertCmd.CommandText = "INSERT INTO CURSOS (Nome, Periodo, Categoria) VALUES (@nome, @per, @cat)";
+
+            
+            insertCmd.Parameters.Add(new SqlParameter("nome", "POO3"));
+
+            insertCmd.Parameters.Add(new SqlParameter("per", 30));
+
+            insertCmd.Parameters.Add(new SqlParameter("cat", "Maneiro23"));
+
+            insertCmd.ExecuteNonQuery();
+
+            conexao.Close();
+
+        }
+    }
+
+}
+
